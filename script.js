84dfb9c8505733629e4771699a3757c1bb30ca74
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-08-06 12:49:54 -0400

    Calculator is functional

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let secondNum = '';
 let operator = null;
 
 console.log(add(1, 2)+" expected 3");
+console.log(add(1, 0.5)+" expected 1.5");
 console.log(subtract(1, 2)+" expected -1");
 console.log(multiply(1, 2)+" expected 2");
 console.log(divide(1, 2)+" expected 0.5");
@@ -18,10 +19,49 @@ const display = document.querySelector('#displayContent')
 
 btns.forEach(btn => {
     btn.addEventListener(('click'), () => {
-        if(btn.className === "btnOperator"){
+        if(operator == '/' && secondNum == '0'){
+            display.textContent = '..nope';
+            firstNum = '';
+            secondNum = ''; 
+            operator = null;
+        }else if(btn.className === "btnOperator" && secondNum != ''){
+            console.log("Inputed a new operator")
+            display.textContent = operate(parseFloat(firstNum),operator,parseFloat(secondNum)).toFixed(3);
+            firstNum = display.textContent;
+            secondNum = '';
+            operator = btn.textContent;
+        }else if(btn.className === "btnOperator"){
             operator = btn.textContent;
         }else if(btn.className === "btnEqual"){
-            display.textContent = operate(parseInt(firstNum),operator,parseInt(secondNum));
+            display.textContent = operate(parseFloat(firstNum),operator,parseFloat(secondNum)).toFixed(3);
+            firstNum = display.textContent;
+            secondNum = '';
+            operator = null;
+        }else if(btn.className === "btnClear"){
+            display.textContent = '';
+            firstNum = '';
+            secondNum = ''; 
+            operator = null;
+        }else if(btn.className === "btnPosNeg"){
+            console.log("pos neg")
+            if(secondNum == ''){
+                firstNum = (parseFloat(firstNum)*(-1)).toFixed(3);
+                display.textContent = firstNum;
+            }else{
+                secondNum = (parseFloat(secondNum)*(-1)).toFixed(3);
+                display.textContent = secondNum;
+            }
+        }else if(btn.className === "btnBack"){
+            
+            if(secondNum == ''){
+                console.log("backspace f n"+firstNum.slice(0, (firstNum.length-1)))
+                firstNum = firstNum.slice(0, (firstNum.length-1));
+                display.textContent = firstNum;
+            }else{
+                console.log("backspace s n"+ secondNum.slice(0, (secondNum.length-1)))
+                secondNum = secondNum.slice(0, secondNum.length-1);
+                display.textContent = secondNum;
+            }
         }else if(operator === null){
             firstNum += btn.textContent;
             display.textContent = firstNum;
